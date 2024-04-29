1aa61cb7b17a377bf501898bb3bc62b410e2b70b
wafas-repo <wafa.qazi@outlook.com>
2024-04-29 00:05:25 -0400

    Spinner

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 import HeroImage from './HeroImage/HeroImage'
 import Grid from './Grid/Grid'
 import Thumb from './Thumb/Thumb'
+import { Spinner } from './Spinner/Spinner.styles'
 // Hook
 import { useHomeFetch } from '../Hooks/useHomeFetch'
 
@@ -42,7 +43,7 @@ export const Home = () => {
                 />
             ))}
       </Grid>
-   
+      <Spinner />
         
     </>
     
